5ce1f6a8ac09c7c6e852ff53b7c7e9c4b575d7b0
Amey Sanyal <amzicom123@gmail.com>
2024-12-07 20:24:54 +0530

    selectordefinition

--- html-to-text.d.ts
+++ html-to-text.d.ts
@@ -1,8 +1,16 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module "html-to-text" {
   export interface HtmlToTextOptions {
     wordwrap?: number | false;
     preserveNewlines?: boolean;
+    selectors?: SelectorDefinition[]; // Include SelectorDefinition if used in options
     // Add other options you might be using
+  }
+
+  export interface SelectorDefinition {
+    selector: string; // CSS selector, e.g., "p", "div"
+    format?: string; // Custom format name
+    options?: Record<string, any>; // Custom options for the format
   }
 
   export function convert(html: string, options?: HtmlToTextOptions): string;
