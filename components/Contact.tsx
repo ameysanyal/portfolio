4c1f9f59a3e9fa1b9726e12732b84391561410c7
Amey Sanyal <amzicom123@gmail.com>
2024-12-07 18:05:55 +0530

    deployment eslint error fix

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -42,6 +42,7 @@ export default function Contact() {
       <form
         className="mt-10 flex flex-col dark:text-black"
         action={async (formData) => {
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
           const { data, error } = await sendEmail(formData);
 
           if (error) {
